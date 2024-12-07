8b7962bf3eecdc53040bb3cafd00e86a68935600
Isaiah Andrade <isaiah_andrade@student.uml.edu>
2024-12-06 20:13:46 -0500

    store password as string hash instead of string

--- FlashCarsPrototype/Assets/Scripts/Account.cs
+++ FlashCarsPrototype/Assets/Scripts/Account.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.Security.Cryptography;
+using System.Text;
+using System;
 public class Account : MonoBehaviour
 {
     public static string username;
     private string firstName;
     private string lastName;
-    private static string password;
+    private string passwordHash;
     private static int wins;
 
     public InputField first;
@@ -20,11 +23,11 @@ public class Account : MonoBehaviour
         firstName = first.text;
         lastName = last.text;
         username = user.text;
-        password = pass.text;
+        passwordHash = HashPassword(pass.text);
 
         // save account for goto login scene
         PlayerPrefs.SetString("username", username);
-        PlayerPrefs.SetString("password", password);
+        PlayerPrefs.SetString("password", passwordHash);
         PlayerPrefs.Save();
         
         // goto login scene
@@ -35,24 +38,41 @@ public class Account : MonoBehaviour
     {
         // get saved username and password
         string savedUsername = PlayerPrefs.GetString("username");
-        string savedPassword = PlayerPrefs.GetString("password");
+        string savedPasswordHash = PlayerPrefs.GetString("password");
+
+        // hash text password to check for match
+        string checkHash = HashPassword(pass.text);
 
         // check for match
-        if (user.text == savedUsername && pass.text == savedPassword)
+        if (user.text == savedUsername && checkHash == savedPasswordHash)
         {
             username = user.text;
-            password = pass.text;
 
             // goto MainMenu scene
             SceneManager.LoadScene("MainMenu");
-
-
         }
         else
         {
             Debug.Log("Invalid username or password");
         }
         
+    }
+
+    private string HashPassword(string password)
+    {
+        using (SHA256 sha256 = SHA256.Create())
+        {
+            // convert to bytes
+            byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            // convert bytes to string
+            StringBuilder hashSB = new StringBuilder();
+            foreach (byte b in hashBytes)
+            {
+                hashSB.Append(b.ToString());
+            }
+            return hashSB.ToString();
+        }
     }
 
     // OnEnable() called when MainMenu is loaded to display username
